2e6fa0c9b4041c9ff7c4d184bed6f9b8d14a386b
Jack Phantom <Jack Phantom@DESKTOP-TMDF6O6>
2021-09-14 18:34:22 +0300

    Добавьте файлы проекта.

---
+++ SortParallelism/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortParallelism
+{
+    public partial class Form1 : Form
+    {
+        private int size;
+        private int[] DirtArray;
+        private int[] ClearArray;
+        private Random rnd = new Random();
+        private Sort S;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void CreateArray(int size)
+        {
+            
+            DirtArray = new int[size];
+            for (int i = 0; i < DirtArray.Length; i++)
+            {
+                DirtArray[i] = rnd.Next(0, 9);
+            }
+            S = new Sort(DirtArray);
+        }
+        public void OutputArray()
+        {
+            
+            Array.Copy(DirtArray, ClearArray, size);
+            for (int i = 0; i < size; i++)
+            {
+                if (S.kindsort == "bubble")
+                {
+                    textBox1.Text += ClearArray[i].ToString();
+                }
+                else if (S.kindsort == "shaker")
+                {
+                    textBox2.Text += ClearArray[i].ToString();
+                }
+                else if (S.kindsort == "shell")
+                {
+                    textBox3.Text += ClearArray[i].ToString();
+                }
+                Console.Write(ClearArray[i]);
+            }
+            Console.WriteLine();
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            size = Convert.ToInt32(textBox4.Text);
+            ClearArray = new int[size];
+            CreateArray(size);
+            S.BubbleSort();
+            OutputArray();
+            S.ShakerSort();
+            OutputArray();
+            S.ShellSort();
+            OutputArray();
+        }
+
+        
+    }
+}
---
+++ SortParallelism/Sort.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortParallelism
+{
+    class Sort
+    {
+        private int[] array;
+        public string kindsort;
+        public Sort(int[] Array)
+        {
+            array = Array;
+        }
+        public int[] BubbleSort()
+        {
+            kindsort = "bubble";
+            var len = array.Length;
+            for (var i = 1; i < len; i++)
+            {
+                for (var j = 0; j < len - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        public int[] ShakerSort()
+        {
+            kindsort = "shaker";
+            for (var i = 0; i < array.Length / 2; i++)
+            {
+                var swapFlag = false;
+                for (var j = i; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapFlag = true;
+                    }
+                }
+
+                for (var j = array.Length - 2 - i; j > i; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapFlag = true;
+                    }
+                }
+
+                if (!swapFlag)
+                {
+                    break;
+                }
+            }
+            return array;
+        }
+        public int[] ShellSort()
+        {
+            kindsort = "shell";
+            var d = array.Length / 2;
+            while (d >= 1)
+            {
+                for (var i = d; i < array.Length; i++)
+                {
+                    var j = i;
+                    while ((j >= d) && (array[j - d] > array[j]))
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        j = j - d;
+                    }
+                }
+
+                d = d / 2;
+            }
+            return array;
+        }
+    }
+}
